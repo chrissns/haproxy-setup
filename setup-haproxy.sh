f9c0d441b9c74c6f0b7600ffd9cfdcdf327f3f15
Christian Sohns <christiansohns@gmx.net>
2023-11-16 10:40:26 +0100

    Made some fixes

--- setup-haproxy.sh
+++ setup-haproxy.sh
@@ -1,52 +1,65 @@
-echo ""
+echo "############################################################################"
 echo ""
 read -p "Press [Enter] key to start setup..."
 echo ""
 echo ""
 
+echo "############################################################################"
 echo "[Script] Update apt"
 sudo apt update && sudo apt upgrade -y
 
+echo "############################################################################"
 echo "[Script] Install cron"
 apt install cron -y
 
+echo "############################################################################"
 echo "[Script] Install Certbot, pip3 and Cloudflare for Certbot"
 apt update
 apt install certbot python3-pip -y
 pip3 install certbot-dns-cloudflare
 
+echo "############################################################################"
 echo "[Script] Install wget, vim, haproxy, rsync"
 sudo apt -y install wget vim haproxy rsync
 
+echo "############################################################################"
 echo "[Script] Install nvm"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
+echo "############################################################################"
 echo "[Script] Reloading bash"
 source ~/.bashrc
 
+echo "############################################################################"
 echo "[Script] Reloading nvm"
 source ~/.nvm/nvm.sh
 
+echo "############################################################################"
 echo "[Script] Setting up nvm"
 nvm install --lts
 
+echo "############################################################################"
 echo "[Script] Install pm2"
 npm i -g pm2
 
+echo "############################################################################"
 echo "[Script] Install yarn"
 npm i -g yarn
 
+echo "############################################################################"
 echo "[Script] Reloading bash"
 source ~/.bashrc
 
 pm2path=$(command -v pm2)
 echo "pm2 path is ${pm2path}"
 
+echo "############################################################################"
 echo "[Script] Removing old config files"
 sudo rm /etc/letsencrypt/cli.ini
 sudo rm /etc/cron.d/certbot
 sudo rm /etc/haproxy/haproxy.cfg
 
+echo "############################################################################"
 echo "[Script] Creating config directories"
 sudo mkdir -p /etc/letsencrypt/
 sudo mkdir -p /etc/letsencrypt/live
@@ -54,6 +67,7 @@ sudo mkdir -p /etc/ssl/haproxycert/
 sudo mkdir -p /etc/haproxy/
 sudo mkdir -p /opt/.secrets/certbot/
 
+echo "############################################################################"
 echo "[Script] Creating config files"
 sudo touch /opt/.secrets/certbot/cloudflare.ini
 chown -R $USER /opt/.secrets/
@@ -71,12 +85,13 @@ touch /etc/haproxy/errors/502.http
 touch /etc/haproxy/errors/503.http
 touch /etc/haproxy/errors/504.http
 
+echo "############################################################################"
 echo "[Script] Install mustache"
 curl -sSL https://git.io/get-mo -o mo
 . "./mo"
 echo "[Script] Mustache was installed successfully" | mo
 
-echo ""
+echo "############################################################################"
 echo "[Script] You will now be asked to enter your setup configuration."
 
 read -p 'Which email do you want to use for ssl certificates? > ' certbotMail
@@ -87,13 +102,14 @@ echo ""
 echo "[Script] Your certbot mail address: ${certbotMail}"
 echo "[Script] Your domains: ${domains}"
 
+echo "############################################################################"
 echo "[Script] Setting up Cloudflare Certbot..."
 echo "[Script] Please create a restricted token with the \"Zone:DNS:Edit\" permissions"
 read -p 'Enter your cloudflare token > ' cloudflareToken
-
 echo "Token will be saved to ~/.secrets/certbot/cloudflare.ini"
 echo "dns_cloudflare_api_token = ${cloudflareToken}" > /opt/.secrets/certbot/cloudflare.ini
 
+echo "############################################################################"
 echo "[Script] Registering following domains:"
 domainsArr=($domains)
 mainDomain=${domainsArr[0]}
@@ -107,21 +123,26 @@ done
 mkdir -p /etc/letsencrypt/live/${mainDomain}/
 echo "export CERTS_DIR=\"/etc/letsencrypt/live/${mainDomain}/\"" >> ~/.bashrc
 
-echo "[Script] Creating past renew hook"
-sudo touch /opt/past-renew-hook.sh
-sudo chown $USER /opt/past-renew-hook.sh
-sudo cat ./scripts/past-renew-hook.sh | mo > /opt/past-renew-hook.sh
+echo "############################################################################"
+echo "[Script] Creating post renew hook"
+sudo touch /opt/post-renew.sh
+sudo chown $USER /opt/post-renew.sh
+sudo cat ./scripts/post-renew.sh | mo > /opt/post-renew.sh
 
+echo "############################################################################"
 echo "[Script] Configuring certbot"
 echo "[Script] Setting up with main domain ${mainDomain}"
 $certbotCmd
 
+echo "############################################################################"
 echo "[Script] Reloading HAProxy"
 systemctl restart haproxy
 
+echo "############################################################################"
 echo "[Script] To make sure all your backends are getting started on boot please follow those steps:"
 pm2 startup
 
+echo "############################################################################"
 echo "Generating ssh-key for GitHub actions"
 mkdir -p ~/.ssh/
 ssh-keygen -m PEM -t rsa -b 4096 -f ~/.ssh/github-actions
