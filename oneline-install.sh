46b63c7cc008874b71c7db8a01c4038b34976102
Christian Sohns <christiansohns@gmx.net>
2023-11-16 10:44:01 +0100

    Added colors

--- oneline-install.sh
+++ oneline-install.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-echo "[Oneline Installer] Using simple oneline setup process."
+GREEN="\033[0;32m"
+
+echo "${GREEN}[Oneline Installer] Using simple oneline setup process."
 
 cd /tmp/
-echo "[Oneline Installer] Checking if an old version of the installer is cached."
+echo "${GREEN}[Oneline Installer] Checking if an old version of the installer is cached."
 sudo rm -rf /tmp/haproxy-setup/
 
-echo "[Oneline Installer] Checking git installation."
+echo "${GREEN}[Oneline Installer] Checking git installation."
 if ! command -v git &> /dev/null
 then
-    echo "[Oneline Installer] Git not found, installing..."
+    echo "${GREEN}[Oneline Installer] Git not found, installing..."
     sudo apt install git -y
 else
-    echo "[Oneline Installer] Git found."
+    echo "${GREEN}[Oneline Installer] Git found."
 fi
 
-echo "[Oneline Installer] Cloning repository..."
+echo "${GREEN}[Oneline Installer] Cloning repository..."
 git clone https://github.com/chrissns/haproxy-setup.git /tmp/haproxy-setup/
-echo "[Oneline Installer] Repository cloned."
+echo "${GREEN}[Oneline Installer] Repository cloned."
 
-echo "[Oneline Installer] Running installation process..."
+echo "${GREEN}[Oneline Installer] Running installation process..."
 cd /tmp/haproxy-setup
 bash /tmp/haproxy-setup/setup-haproxy.sh
-echo "[Oneline Installer] Installation process completed."
+echo "${GREEN}[Oneline Installer] Installation process completed."
--- setup-haproxy.sh
+++ setup-haproxy.sh
@@ -1,3 +1,5 @@
+GREEN="\033[0;32m"
+
 echo "############################################################################"
 echo ""
 read -p "Press [Enter] key to start setup..."
@@ -5,62 +7,62 @@ echo ""
 echo ""
 
 echo "############################################################################"
-echo "[Script] Update apt"
+echo "${GREEN}[Script] Update apt"
 sudo apt update && sudo apt upgrade -y
 
 echo "############################################################################"
-echo "[Script] Install cron"
+echo "${GREEN}[Script] Install cron"
 apt install cron -y
 
 echo "############################################################################"
-echo "[Script] Install Certbot, pip3 and Cloudflare for Certbot"
+echo "${GREEN}[Script] Install Certbot, pip3 and Cloudflare for Certbot"
 apt update
 apt install certbot python3-pip -y
 pip3 install certbot-dns-cloudflare
 
 echo "############################################################################"
-echo "[Script] Install wget, vim, haproxy, rsync"
+echo "${GREEN}[Script] Install wget, vim, haproxy, rsync"
 sudo apt -y install wget vim haproxy rsync
 
 echo "############################################################################"
-echo "[Script] Install nvm"
+echo "${GREEN}[Script] Install nvm"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 echo "############################################################################"
-echo "[Script] Reloading bash"
+echo "${GREEN}[Script] Reloading bash"
 source ~/.bashrc
 
 echo "############################################################################"
-echo "[Script] Reloading nvm"
+echo "${GREEN}[Script] Reloading nvm"
 source ~/.nvm/nvm.sh
 
 echo "############################################################################"
-echo "[Script] Setting up nvm"
+echo "${GREEN}[Script] Setting up nvm"
 nvm install --lts
 
 echo "############################################################################"
-echo "[Script] Install pm2"
+echo "${GREEN}[Script] Install pm2"
 npm i -g pm2
 
 echo "############################################################################"
-echo "[Script] Install yarn"
+echo "${GREEN}[Script] Install yarn"
 npm i -g yarn
 
 echo "############################################################################"
-echo "[Script] Reloading bash"
+echo "${GREEN}[Script] Reloading bash"
 source ~/.bashrc
 
 pm2path=$(command -v pm2)
 echo "pm2 path is ${pm2path}"
 
 echo "############################################################################"
-echo "[Script] Removing old config files"
+echo "${GREEN}[Script] Removing old config files"
 sudo rm /etc/letsencrypt/cli.ini
 sudo rm /etc/cron.d/certbot
 sudo rm /etc/haproxy/haproxy.cfg
 
 echo "############################################################################"
-echo "[Script] Creating config directories"
+echo "${GREEN}[Script] Creating config directories"
 sudo mkdir -p /etc/letsencrypt/
 sudo mkdir -p /etc/letsencrypt/live
 sudo mkdir -p /etc/ssl/haproxycert/
@@ -68,7 +70,7 @@ sudo mkdir -p /etc/haproxy/
 sudo mkdir -p /opt/.secrets/certbot/
 
 echo "############################################################################"
-echo "[Script] Creating config files"
+echo "${GREEN}[Script] Creating config files"
 sudo touch /opt/.secrets/certbot/cloudflare.ini
 chown -R $USER /opt/.secrets/
 chmod 600 /opt/.secrets/certbot/cloudflare.ini
@@ -86,31 +88,31 @@ touch /etc/haproxy/errors/503.http
 touch /etc/haproxy/errors/504.http
 
 echo "############################################################################"
-echo "[Script] Install mustache"
+echo "${GREEN}[Script] Install mustache"
 curl -sSL https://git.io/get-mo -o mo
 . "./mo"
-echo "[Script] Mustache was installed successfully" | mo
+echo "${GREEN}[Script] Mustache was installed successfully" | mo
 
 echo "############################################################################"
-echo "[Script] You will now be asked to enter your setup configuration."
+echo "${GREEN}[Script] You will now be asked to enter your setup configuration."
 
 read -p 'Which email do you want to use for ssl certificates? > ' certbotMail
 read -p 'Which domains should be configured? (seperated by spaces) > ' domains
 
 echo ""
 
-echo "[Script] Your certbot mail address: ${certbotMail}"
-echo "[Script] Your domains: ${domains}"
+echo "${GREEN}[Script] Your certbot mail address: ${certbotMail}"
+echo "${GREEN}[Script] Your domains: ${domains}"
 
 echo "############################################################################"
-echo "[Script] Setting up Cloudflare Certbot..."
-echo "[Script] Please create a restricted token with the \"Zone:DNS:Edit\" permissions"
+echo "${GREEN}[Script] Setting up Cloudflare Certbot..."
+echo "${GREEN}[Script] Please create a restricted token with the \"Zone:DNS:Edit\" permissions"
 read -p 'Enter your cloudflare token > ' cloudflareToken
 echo "Token will be saved to ~/.secrets/certbot/cloudflare.ini"
 echo "dns_cloudflare_api_token = ${cloudflareToken}" > /opt/.secrets/certbot/cloudflare.ini
 
 echo "############################################################################"
-echo "[Script] Registering following domains:"
+echo "${GREEN}[Script] Registering following domains:"
 domainsArr=($domains)
 mainDomain=${domainsArr[0]}
 cetrbotCmd="sudo certbot certonly --dns-cloudflare --dns-cloudflare-credentials /opt/.secrets/certbot/cloudflare.ini --dns-cloudflare-propagation-seconds 30 --email ${certbotMail} --agree-tos"
@@ -124,22 +126,22 @@ mkdir -p /etc/letsencrypt/live/${mainDomain}/
 echo "export CERTS_DIR=\"/etc/letsencrypt/live/${mainDomain}/\"" >> ~/.bashrc
 
 echo "############################################################################"
-echo "[Script] Creating post renew hook"
+echo "${GREEN}[Script] Creating post renew hook"
 sudo touch /opt/post-renew.sh
 sudo chown $USER /opt/post-renew.sh
 sudo cat ./scripts/post-renew.sh | mo > /opt/post-renew.sh
 
 echo "############################################################################"
-echo "[Script] Configuring certbot"
-echo "[Script] Setting up with main domain ${mainDomain}"
+echo "${GREEN}[Script] Configuring certbot"
+echo "${GREEN}[Script] Setting up with main domain ${mainDomain}"
 $certbotCmd
 
 echo "############################################################################"
-echo "[Script] Reloading HAProxy"
+echo "${GREEN}[Script] Reloading HAProxy"
 systemctl restart haproxy
 
 echo "############################################################################"
-echo "[Script] To make sure all your backends are getting started on boot please follow those steps:"
+echo "${GREEN}[Script] To make sure all your backends are getting started on boot please follow those steps:"
 pm2 startup
 
 echo "############################################################################"
